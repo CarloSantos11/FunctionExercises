480dee970d6e2a462a35e136c55ce5caba786b5e
Carlo Santos <carlo@snapit.solutions>
2020-09-26 08:33:38 -0500

    created 9 exercises to grasp functions

---
+++ script.js
@@ -0,0 +1,62 @@
+// FUNCTION EXERCISES
+
+// Remember the syntax for a function is
+
+// function nameOfFuntion(param1, param2) {
+//    code block();
+// }
+//
+
+// **************************************************** //
+// #1 Write a function that adds two numbers together. 
+// **************************************************** //
+
+// **************************************************** //
+// #2 Write a function that squares a number
+// **************************************************** //
+
+// **************************************************** //
+// #3 Write a function that cubes a number
+// **************************************************** //
+
+// **************************************************** //
+// #4 Write a function that takes two arguments both numbers.
+//    The first number will be raised to the power of the second number.
+// **************************************************** //
+
+// **************************************************** //
+// #5
+// We are building an early feature for NASA. 
+// This feature requires us to 
+// create a function that finds
+// the hypotenuse of a triangle.
+
+// The formula to find the 
+// hypotenuse is. 
+// h^2  = a^2 + b^2 
+// **************************************************** //
+
+// **************************************************** //
+// #6 
+// Write a function called isBoolean that takes in a boolean param, 
+// meaning that when we pass in an argument it will be a true or false value. If we pass in a truthy value we will print to the console “this is a truthy value”. If we pass a falsy value we will print to the console “this is a falsy value”
+// **************************************************** //
+
+// **************************************************** //
+// #7 
+// Write a function that takes no parameters and only prints “Hello” to the console 100 times. 
+// **************************************************** //
+
+// **************************************************** //
+// #8 
+// Write a function that takes no parameters. The function will prompt the user to enter the password. The secret password is “password”.
+// **************************************************** //
+
+// **************************************************** //
+// #9
+// Write a program that prints the numbers from 1 to 100. 
+// But for multiples of three print “Fizz” instead of the 
+// number and for the multiples of five print “Buzz”. For 
+// numbers which are multiples of both three and five print 
+// “FizzBuzz"
+// **************************************************** //
